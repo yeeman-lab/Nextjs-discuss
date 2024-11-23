8252c192e876413ae47caa418874382c3fefbf7a
Keung, Yee Man <yeem.kg@gmail.com>
2024-11-23 10:13:44 +0000

    Change env variable name

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,8 @@ import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./db";
 
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GITHUB_CLIENT_ID = process.env.NEXTJS_GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.NEXTJS_GITHUB_CLIENT_SECRET;
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
   throw new Error();
